d3aada2c098096bc77a99cf8bc1a6ef6bc973a15
Adam Popławski <adam.poplawski@asseco.pl>
2023-04-14 12:18:03 +0200

    szukanie

--- libs/music/src/lib/songs/songs.page.ts
+++ libs/music/src/lib/songs/songs.page.ts
@@ -2,24 +2,34 @@ import { ChangeDetectionStrategy, Component, Input, ViewChild, inject } from '@a
 import { SongsStore } from './songs.store';
 import { Song } from '@asseco/api-client';
 import { MatSort } from '@angular/material/sort';
+import { UntypedFormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'asseco-songs',
   templateUrl: './songs.page.html',
   styleUrls: ['./songs.page.css'],
   // changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [
-    SongsStore,
-  ],
+  providers: [SongsStore],
 })
 export class SongsPage {
+  [x: string]: any;
   store = inject(SongsStore);
 
   rows = 4;
 
+  private fb = inject(UntypedFormBuilder);
+
+  filtersForm = this.fb.group({
+    q: ['query', [], []],
+    year: ['year', [], []],
+  });
+
   ngOnInit(): void {
     this.store.init();
   }
-
+  submitFilters() {
+    const filters= this.filtersForm.value;
+   // this.store.filters = filtersForm.value;
+  }
   
 }
--- libs/music/src/lib/songs/songs.store.ts
+++ libs/music/src/lib/songs/songs.store.ts
@@ -1,104 +1,118 @@
-import { ComponentStore, tapResponse } from "@ngrx/component-store";
-import { Injectable, inject } from "@angular/core";
-import { Artist, Song } from "@asseco/api-client";
-import { Observable, delay, switchMap } from "rxjs";
-import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { Sort } from "@angular/material/sort";
-import { PageEvent } from "@angular/material/paginator";
-import { tapStore } from "../data.store";
+import { ComponentStore, tapResponse } from '@ngrx/component-store';
+import { Injectable, inject } from '@angular/core';
+import { Artist, Song } from '@asseco/api-client';
+import { Observable, delay, switchMap } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Sort } from '@angular/material/sort';
+import { PageEvent } from '@angular/material/paginator';
+import { tapStore } from '../data.store';
 
 export interface SongsState {
-    songs: Song[];
-    sort: Sort;
-    page: PageEvent;
-    error?: HttpErrorResponse;
-    loading: boolean;
+  songs: Song[];
+  sort: Sort;
+  page: PageEvent;
+  error?: HttpErrorResponse;
+  loading: boolean;
+  filters: SongFilters;
 }
-
+export interface SongFilters {
+  q?: string;
+  year?: string;
+}
 const initState: SongsState = {
-    loading: false,
-    songs: [],
-    sort: {
-        active: 'title',
-        direction: 'asc',
-    },
-    page: {
-        length: 40,
-        pageIndex: 0,
-        pageSize: 5,
-    }
-}
+  loading: false,
+  songs: [],
+  sort: {
+    active: 'title',
+    direction: 'asc',
+  },
+  page: {
+    length: 40,
+    pageIndex: 0,
+    pageSize: 5,
+  },
+  filters: {},
+};
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class SongsStore extends ComponentStore<SongsState> {
+  private baseUrl = 'http://localhost:3000';
+  private http = inject(HttpClient);
 
-    private baseUrl = 'http://localhost:3000';
-    private http = inject(HttpClient);
+  loading$ = this.select((s) => s.loading);
+  songs$ = this.select((s) => s.songs);
+  page$ = this.select((s) => s.page);
+  sort$ = this.select((s) => s.sort);
 
-    loading$ = this.select(s => s.loading);
-    songs$ = this.select(s => s.songs);
-    page$ = this.select(s => s.page);
-    sort$ = this.select(s => s.sort);
-
-    columns = ['id', 
-    'title', 
+  columns = [
+    'id',
+    'title',
     'year', //'artist', 'url'
-];
-    
-    constructor() {
-        super(initState);
-        this.state$.subscribe(s => console.log(s))
-    }
+  ];
 
-    set sort(sort: Sort) {
-        this.patchState({
-            sort,
-            page: {
-                ...this.page,
-                pageIndex: 0,
-            }
-        });
-        this.init();
-    }
+  constructor() {
+    super(initState);
+    this.state$.subscribe((s) => console.log(s));
+  }
 
-    get sort(): Sort {
-        return this.get().sort;
-    }
-
-    set page(page: PageEvent) {
-        this.patchState({page});
-        this.init();
-    }
-
-    get page(): PageEvent {
-        return this.get().page;
-    }
-
-    readonly init = this.effect((data$: Observable<void>) => {
-        return data$.pipe(
-            switchMap(() => {
-                const params = {
-                    _sort: this.sort.active,
-                    _order: this.sort.direction,
-                    _page: this.page.pageIndex+1,
-                    _limit: this.page.pageSize,
-                }
-                return this.http.get<Song[]>(this.baseUrl+'/songs', { params, observe: 'response' }).pipe(
-                    tapStore(
-                        this,
-                        (res) => this.patchState({
-                            songs: res.body || [],
-                            page: {
-                                ...this.page,
-                                length: parseInt(res.headers.get('x-total-count') || '0')
-                            }
-                        }),
-                    ),
-                )
-            }),
-        );
+  set sort(sort: Sort) {
+    this.patchState({
+      sort,
+      page: {
+        ...this.page,
+        pageIndex: 0,
+      },
     });
+    this.init();
+  }
 
+  get sort(): Sort {
+    return this.get().sort;
+  }
+
+  set page(page: PageEvent) {
+    this.patchState({ page });
+    this.init();
+  }
+
+  get page(): PageEvent {
+    return this.get().page;
+  }
+
+  set filters(filters: SongFilters) {
+    this.patchState({ filters });
+    this.init();
+  }
+
+  get filters(): SongFilters {
+    return this.get().filters;
+  }
+  readonly init = this.effect((data$: Observable<void>) => {
+    return data$.pipe(
+      switchMap(() => {
+        const params = {
+          ...this.get().filters,
+          _sort: this.sort.active,
+          _order: this.sort.direction,
+          _page: this.page.pageIndex + 1,
+          _limit: this.page.pageSize,
+        };
+        return this.http
+          .get<Song[]>(this.baseUrl + '/songs', { params, observe: 'response' })
+          .pipe(
+            tapStore(this, (res) =>
+              this.patchState({
+                songs: res.body || [],
+                page: {
+                  ...this.page,
+                  length: parseInt(res.headers.get('x-total-count') || '0'),
+                },
+              })
+            )
+          );
+      })
+    );
+  });
 }
